7a5e26384e8b913fbb96d8669fb99bb2478e79bb
unknown <zhury10@chinaunicom.cn>
2023-07-03 11:31:09 +0800

    .

---
+++ src/components/custom/GithubSite.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="text-neutral-400">
+    <span>Forked and modified from </span>
+    <!-- 
+    <a href="https://github.com/zhu18/ANOV-GPT-web" target="_blank" class="text-blue-500">
+      zhu18/ANOV-GPT-web
+    </a> -->
+  </div>
+</template>
---
+++ src/views/chat/components/Message/Avatar.vue
@@ -0,0 +1,33 @@
+<script lang="ts" setup>
+import { computed } from 'vue'
+import { NAvatar } from 'naive-ui'
+import { useUserStore, useAppStore } from '@/store'
+import { isString } from '@/utils/is'
+
+
+interface Props {
+  image?: boolean
+}
+defineProps<Props>()
+
+const userStore = useUserStore()
+
+const avatar = computed(() => userStore.userInfo.avatar)
+
+const appStore = useAppStore()
+
+const theme = computed(() => appStore.theme)
+
+
+console.log(appStore)
+</script>
+
+<template>
+  <template v-if="image">
+    <NAvatar v-if="isString(avatar) && avatar.length > 0" :src="avatar" />
+    <NAvatar v-else round />
+  </template>
+  <span v-else class="text-[28px] dark:text-white">        
+     <img style="width: 28px;" :src="theme=='dark'?'/logo.png':'/logo-dark.png'">
+  </span>
+</template>
